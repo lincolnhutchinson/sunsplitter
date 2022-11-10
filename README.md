2314ac9e92dc97db851e25bfe81a281258ed6861
Lincoln <117424099+codelincoln@users.noreply.github.com>
2022-11-10 04:44:43 -0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sunsplitter System
+
+A ttrpg system made for me and my friends. Using Worlds Without Number as a starting point, and asacolips' [Boilerplate System](https://gitlab.com/asacolips-projects/foundry-mods/boilerplate) as the initial foundry code.
---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const SUNSPLITTER = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SUNSPLITTER.abilities = {
+  "str": "SUNSPLITTER.AbilityStr",
+  "dex": "SUNSPLITTER.AbilityDex",
+  "con": "SUNSPLITTER.AbilityCon",
+  "int": "SUNSPLITTER.AbilityInt",
+  "wis": "SUNSPLITTER.AbilityWis",
+  "cha": "SUNSPLITTER.AbilityCha"
+};
+
+SUNSPLITTER.abilityAbbreviations = {
+  "str": "SUNSPLITTER.AbilityStrAbbr",
+  "dex": "SUNSPLITTER.AbilityDexAbbr",
+  "con": "SUNSPLITTER.AbilityConAbbr",
+  "int": "SUNSPLITTER.AbilityIntAbbr",
+  "wis": "SUNSPLITTER.AbilityWisAbbr",
+  "cha": "SUNSPLITTER.AbilityChaAbbr"
+};
